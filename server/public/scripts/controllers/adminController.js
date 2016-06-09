28e8361e8531e5ba72d62ad92c5646d8bbe7f855
Daniel Chavez <d.chavez562@yahoo.com>
2016-06-09 16:25:13 -0500

    descriptions and controller modifications

--- server/public/scripts/controllers/adminController.js
+++ server/public/scripts/controllers/adminController.js
@@ -5,15 +5,7 @@ myApp.controller('AdminController', ['$scope', '$http', 'DataFactory', function(
   $scope.favorites = [];
   $scope.favCount = 0;
 
-  // if($scope.dataFactory.factoryGetFavorites() === undefined) {
-  //   $scope.dataFactory.factoryRefreshFavoriteData().then(function() {
-  //     $scope.favorites = $scope.dataFactory.factoryGetFavorites();
-  //     $scope.favCount = $scope.favorites.length;
-  //   });
-  // } else {
-  //   $scope.favorites = $scope.dataFactory.factoryGetFavorites();
-  //   $scope.favCount = $scope.favorites.length;
-  // }
-  
+
+
 
 }]);
--- server/public/scripts/controllers/orderController.js
+++ server/public/scripts/controllers/orderController.js
@@ -1,11 +1,23 @@
 myApp.controller('OrderController', ['$scope', '$http', 'DataFactory', function($scope, $http, DataFactory) {
   console.log('order controller running');
-  //scope variable & injection
+  //scope variables & injection
   $scope.dataFactory = DataFactory;
   $scope.currentOrder = {};
   $scope.grilledCheeses = $scope.dataFactory.grilledCheeses;
   $scope.gcCount = 0;
+  $scope.currentOrder.gc1 = 0;
+  $scope.currentOrder.gc2 = 0;
+  $scope.currentOrder.gc3 = 0;
+  $scope.currentOrder.gc4 = 0;
   $scope.currentOrder.gcTotal = 0;
+  $scope.quantity = [0,1,2,3,4,5,6,7,8,9,10];
+
+  $scope.addGC = function () {
+    $scope.currentOrder.gcTotal = (($scope.currentOrder.gc1 * $scope.grilledCheeses.gc1.price) +
+                                   ($scope.currentOrder.gc2 * $scope.grilledCheeses.gc2.price) +
+                                   ($scope.currentOrder.gc3 * $scope.grilledCheeses.gc3.price) +
+                                   ($scope.currentOrder.gc4 * $scope.grilledCheeses.gc4.price));
+  };
 
   $scope.submitOrder = function () {
   var data = $scope.currentOrder;
@@ -22,14 +34,6 @@ myApp.controller('OrderController', ['$scope', '$http', 'DataFactory', function(
   $location.url('/thank-you');
   };
 
-  // if($scope.dataFactory.factoryGetFavorites() === undefined) {
-  //   $scope.dataFactory.factoryRefreshFavoriteData().then(function() {
-  //     $scope.favorites = $scope.dataFactory.factoryGetFavorites();
-  //     $scope.favCount = $scope.favorites.length;
-  //   });
-  // } else {
-  //   $scope.favorites = $scope.dataFactory.factoryGetFavorites();
-  //   $scope.favCount = $scope.favorites.length;
-  // }
+
 
 }]);
