24c95bba6f7d1eea5ba2faed03683a91d99323e3
Daniel Chavez <d.chavez562@yahoo.com>
2016-07-19 11:17:43 -0500

    started adding all vedors and authentication to match, routes, req, and modules

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+//passport
+var session = require('express-session');
+var passport = require('./auth/passport');
+var isLoggedIn = require('./utils/auth');
 //route variables
-var index = require('./routes/index');
-var orders = require('./routes/orders');
 var connection = require('./modules/connection');
 console.log(connection);
+var login = require('./routes/login');
+var index = require('./routes/index');
+var orders = require('./routes/orders');
+
+
+
 //set port
 app.set('port', process.env.PORT || 5000);
 
@@ -16,9 +24,14 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
 //modules n routes
+app.use('/login', login);
 app.use('/orders', orders);
 app.use('/index', index);
+
 //catch all
 app.use('/', index);
 
