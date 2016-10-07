079ba891a7ff61d1e1bb16bb951e6d1da2337ce8
Daniel Chavez <d.chavez562@yahoo.com>
2016-10-07 12:30:59 -0500

    front end work. fixed bug causing nav bar issues

--- server/public/assets/scripts/controllers/adminController.js
+++ server/public/assets/scripts/controllers/adminController.js
@@ -1,4 +1,4 @@
-myApp.controller('AdminController', ['$scope', '$http', 'DataFactory', function($scope, $http, DataFactory) {
+myApp.controller('AdminController', ['$scope', '$http', '$window', 'DataFactory', function($scope, $http, $window, DataFactory) {
   console.log('admin controller running');
    //scope variables
 
--- server/public/assets/scripts/factories/dataFactory.js
+++ server/public/assets/scripts/factories/dataFactory.js
@@ -1,26 +1,26 @@
 myApp.factory('DataFactory', ['$http', function($http) {
   console.log('dataFactory running');
 
-  // var grilledCheeses = {
-  //   gc1 : {name :'Uh-huh Honey',
-  //          price : 3,
-  //          description : 'Crisp sliced apples, cheddar cheese, drizzled honey and a sprinkle of cinnamon.'
-  //   },
-  //   gc2 : {name :'Bravo Italiano',
-  //          price : 3,
-  //          description : 'Mozzarella cheese, freshly sliced tomato, with basil pesto sauce.',
-  //   },
-  //   gc3 :  {name :'3 Cheese',
-  //          price : 3,
-  //          description : 'Cheddar, Swiss and Mozzarella cheeses between crisp buttery bread.'
-  //   },
-  //   gc4 :  {name :'Cozy Campfire',
-  //          price : 3,
-  //          description : 'A s\'mores inspired recipe with melted chocolate chips, crushed graham cracker, and toasty marshmallows.'
-  //   }
-  // };
+  var grilledCheeses = {
+    gc1 : {name :'Uh-huh Honey',
+           price : 3,
+           description : 'Crisp sliced apples, cheddar cheese, drizzled honey and a sprinkle of cinnamon.'
+    },
+    gc2 : {name :'Bravo Italiano',
+           price : 3,
+           description : 'Mozzarella cheese, freshly sliced tomato, with basil pesto sauce.',
+    },
+    gc3 :  {name :'3 Cheese',
+           price : 3,
+           description : 'Cheddar, Swiss and Mozzarella cheeses between crisp buttery bread.'
+    },
+    gc4 :  {name :'Cozy Campfire',
+           price : 3,
+           description : 'A s\'mores inspired recipe with melted chocolate chips, crushed graham cracker, and toasty marshmallows.'
+    }
+  };
 
-  // var grilledCheeses = {};
+  var grilledCheeses = {};
 
   function getGrilledCheese() {
     $http.get('/cheese')
